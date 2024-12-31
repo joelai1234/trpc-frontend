44d957aea595c41b9d6b334d16ff1d55632504d0
joe.lai <joelai1234567890@gmail.com>
2025-01-01 02:19:11 +0800

    feat: build

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/joe.lai/Desktop/projects/test/trpg/frontend/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
